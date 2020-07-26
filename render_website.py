eeea1007c9877a648ad287bd0c9e5b43c6456d85
andimeon <andimeon@gmail.com>
2020-07-26 18:37:26 +0900

    remove unused functions

--- render_website.py
+++ render_website.py
@@ -6,28 +6,29 @@ import os
 from urllib import parse
 import glob
 
-BOOTSTRAP_FILE = {
-    'css': 'bootstrap.min.css',
-    'jquery': 'jquery-3.4.1.slim.min.js',
-    'popper': 'popper.min.js',
-    'bootstrap': 'bootstrap.min.js',
+BOOTSTRAP_FILE_PATH = {
+    'css': '../static/bootstrap.min.css',
+    'jquery': '../static/jquery-3.4.1.slim.min.js',
+    'popper': '../static/popper.min.js',
+    'bootstrap': '../static/bootstrap.min.js',
 }
 
 
-def on_reload():
+def render_website_pages():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    books_on_pages = get_books_pages()
-    pages_href = get_pages_links(len(books_on_pages))
+    books_on_pages = list(chunked(BOOKS_METADATA, 10))
+    pages_href = [f'../pages/index{num}.html' for num in range(len(books_on_pages))]
     remove_files()
     
-    for count, books_page in enumerate(books_on_pages):
+    # ниже в цикле задействованы обе переменные функции enumerate: count, books_on_page
+    for count, books_on_page in enumerate(books_on_pages):
         rendered_page = template.render(
-            bootstrap_path=BOOTSTRAP_FILE,
-            books_metadata=list(chunked(books_page, 2)),
+            bootstrap_path=BOOTSTRAP_FILE_PATH,
+            books_metadata=list(chunked(books_on_page, 2)),
             pages_href=pages_href,
             previous_page_href=get_previous_page_link(count, pages_href),
             next_page_href=get_next_page_link(count, pages_href),
@@ -38,12 +39,7 @@ def on_reload():
             file.write(rendered_page)
 
 
-def get_books_pages():
-    global books_metadata
-    return [books_metadata[i: i + 10] for i in range(0, len(books_metadata), 10)]
-
-
-def get_url_quote(books):
+def quote_url(books):
     for count, book in enumerate(books):
         book_path = os.path.join('../', book['book_path'])
         img_path = os.path.join('../', book['img_src'])
@@ -52,29 +48,18 @@ def get_url_quote(books):
     return books
 
 
-def get_pages_links(pages_numbers):
-    return [f'../pages/index{num}.html' for num in range(pages_numbers)]
-
-
 def get_previous_page_link(count, pages_href):
-    if count != 0:
+    if not count:
         return pages_href[count - 1]
     else:
         return None
 
 
 def get_next_page_link(count, pages_href):
-    if count != (len(pages_href) - 1):
+    if count != len(pages_href) - 1:
         return pages_href[count + 1]
     else:
         return None
-
-
-def bootstrap_file_path():
-    BOOTSTRAP_FILE['css'] = os.path.join('../static', BOOTSTRAP_FILE['css'])
-    BOOTSTRAP_FILE['jquery'] = os.path.join('../static', BOOTSTRAP_FILE['jquery'])
-    BOOTSTRAP_FILE['popper'] = os.path.join('../static', BOOTSTRAP_FILE['popper'])
-    BOOTSTRAP_FILE['bootstrap'] = os.path.join('../static', BOOTSTRAP_FILE['bootstrap'])
 
 
 def remove_files():
@@ -88,10 +73,10 @@ if __name__ == '__main__':
     os.makedirs('pages', exist_ok=True)
     with open('books_metadata.json', 'r') as file:
         books_metadata_json = json.load(file)
-    books_metadata = get_url_quote(books_metadata_json)
-    bootstrap_file_path()
+
+    BOOKS_METADATA = quote_url(books_metadata_json)
     
-    on_reload()
+    render_website_pages()
     server = Server()
-    server.watch('template.html', on_reload)
+    server.watch('template.html', render_website_pages)
     server.serve(root='.')
